3acc0bb20ce36fcbde82967cdf0afcc85f91afea
Kim Fom <kimfom01@gmail.com>
2022-07-24 19:18:35 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        const int SIZE = 10;
+        static void Main(string[] args)
+        {
+            int i,  num;
+            int[] array_demo = new int[SIZE];
+
+            for (i = 0; i <= SIZE - 1; i++)
+            {
+                array_demo[i] = i + 5;
+            }
+            
+            for(i = 0; i <= SIZE - 1; i++)
+            {
+                Console.Write(array_demo[i] + " ");
+            }
+
+            Console.Write("\nEnter the number to search: ");
+            num = Int32.Parse(Console.ReadLine());
+
+            BinarySearch(array_demo, num);
+        }
+        
+        // Binary Search Algorithm
+        static void BinarySearch(int[] haystack, int needle)
+        {
+            int left, middle, right, needle_index;
+            bool found;
+
+            left = 0;
+            right = haystack.Length - 1;
+            found = false;
+            needle_index = -1;
+            while(left <= right && found == false)
+            {
+                middle =(int)((left + right) / 2);
+
+                if(needle < haystack[middle])
+                {
+                    right = middle - 1;
+
+                }
+                else if(needle > haystack[middle])
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    needle_index = middle;
+                    found = true;
+                }
+            }
+
+            if(found == false)
+            {
+                Console.WriteLine("Value not found!");
+            }
+            else
+            {
+                Console.WriteLine("Value found at index " + needle_index);
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Binary Search
+
+This is a simple algorithm to find an element in an ordered array using binary search
+
+Feel free to walk through the project and do whatever you wish 😉
+
+
+
+
+* [Buy Me A Coffee](https://www.buymeacoffee.com/kimfom01)
